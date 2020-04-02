0ae63ceb9f7eed49df8bd26127ac82e2aa57589c
jonathasacruz <jonathas.acruz@gmail.com>
2020-04-02 00:16:39 -0300

    Implementation of  border-radius setting fields

---
+++ script.js
@@ -0,0 +1,5 @@
+function tlBorderSet(){
+    document.getElementById("tr").value = this.document.getElementById("tl").value;
+    document.getElementById("bl").value = this.document.getElementById("tl").value;
+    document.getElementById("br").value = this.document.getElementById("tl").value;
+}

df033c562f6560c6532758e2cabcaeea9eac92da
jonathasacruz <jonathas.acruz@gmail.com>
2020-04-06 21:25:42 -0300

    Border Radius auto update by JS

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-var link;
+let link;
 
 function borderSetLink(){
    link = true;
+   updateBorderRadius();
 }
 
 function borderUnlink(){
    link = false;
+   updateBorderRadius();
 }
 
 function tlBorderSet(){
     if (link === true) {
-        document.getElementById("tr").value = this.document.getElementById("tl").value;
-        document.getElementById("bl").value = this.document.getElementById("tl").value;
-        document.getElementById("br").value = this.document.getElementById("tl").value;
-    }
+        document.getElementById("tr").value = document.getElementById("tl").value;
+        document.getElementById("bl").value = document.getElementById("tl").value;
+        document.getElementById("br").value = document.getElementById("tl").value;
+   }
+   updateBorderRadius();
+}
+
+function updateBorderRadius(){
+    let tl, tr, bl, br;
+    tl = document.getElementById("tl").value + "%";
+    tr = document.getElementById("tr").value + "%";
+    bl = document.getElementById("bl").value + "%";
+    br = document.getElementById("br").value + "%";
+    document.getElementById("border-box").style.borderTopLeftRadius = tl;
+    document.getElementById("border-box").style.borderTopRightRadius = tr;
+    document.getElementById("border-box").style.borderBottomLeftRadius = bl;
+    document.getElementById("border-box").style.borderBottomRightRadius = br;
 }
